7182786f409003dc0d222980adaf2681a47ef834
Mykola Voinikov <qwoxa1@gmail.com>
2023-06-10 11:32:53 +0300

    feat: add capitalize functionality

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-import camelCase from 'voca/camel_case'
-import submarines from '../submarines/index'
+import capitalize from 'voca/capitalize'
 
 function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = camelCase(`count is ${counter}`);
+    element.innerHTML = capitalize(`count is ${counter}`);
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
-
-  console.log(submarines('submarine'));
 }
 
 
